48b58a2333e28a1f4c411abb230c9235f745c025
JuanDominguez <juan@portos.xyz>
2022-03-09 12:10:23 -0600

    Updates

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-import Pane from "./Pane";
-import Item from "./Item";
-import TopBar from "./TopBar";
-import CardInfo from "./CardInfo";
-import HistoryItem from "./HistoryItem";
-import Loading from "./Loading";
-import Error from "./Error";
-
-export { Pane, Item, TopBar, CardInfo, HistoryItem, Loading, Error };
--- src/utils/strings.js
+++
@@ -1,3 +0,0 @@
-export const resizeString = (str, length = 20) => {
-  return str.length > length ? str.substring(0, length) + "…" : str;
-};
