375549f25c9baa733dde6a4974963d3fb336fe85
JuanDominguez <juan@portos.xyz>
2022-03-09 13:44:28 -0600

    sorting countries by name in history

--- src/App.js
+++ src/App.js
@@ -61,6 +61,15 @@ function App() {
       setHistory(newHistory);
     } else {
       newHistory.push(element);
+      newHistory.sort((a, b) => {
+        if (a.name > b.name) {
+          return 1;
+        }
+        if (a.name < b.name) {
+          return -1;
+        }
+        return 0;
+      });
       setHistory(newHistory);
     }
   };
